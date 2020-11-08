973cf47167c04db610608a8266a9477ff8302571
patrickodey419 <patrickodey419@gmail.com>
2020-11-08 22:11:02 +0100

    chore: added projects section

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.type', {
+  strings: ['I write scalable and responsible website and applications',
+            'My communication skills are pretty amazing',
+            'I enjoy skateboarding',
+            "I watch comedy when I'm not coding"
+          ],
+  typeSpeed: 30,
+  backSpeed: 30,
+  loop:      true 
+});
