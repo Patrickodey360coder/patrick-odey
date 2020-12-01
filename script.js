e451ddd85ca584663a1fd95376ed6f1611b18a1e
patrickodey419 <patrickodey419@gmail.com>
2020-12-01 07:56:01 +0100

    fix: corrected contact info

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 //for nav scrool behaviour
-var navbar = document.getElementById('navbar');
-window.onscroll = ()=>{
-  "use strict";
-  if(document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100){
-    navbar.classList.add("nav-colored");
-    navbar.classList.remove("nav-transparent");
-  }else{
-    navbar.classList.remove("nav-colored");
-    navbar.classList.add("nav-transparent");
-  }
-};
+window.onload = (e)=> {
+  var navbar = document.getElementById('navbar');
+  window.onscroll = ()=>{
+    "use strict";
+    if(document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100){
+      navbar.classList.add("nav-colored");
+      navbar.classList.remove("nav-transparent");
+    }else{
+      navbar.classList.remove("nav-colored");
+      navbar.classList.add("nav-transparent");
+    }
+  };
 
-let typed = new Typed('.type', {
-  strings: ['I write scalable and responsible website and applications',
-            'My communication skills are pretty amazing',
-            'I enjoy skateboarding',
-            "I watch comedy when I'm not coding"
-          ],
-  typeSpeed: 30,
-  backSpeed: 30,
-  loop:      true 
-});
-
+  let typed = new Typed('.type', {
+    strings: ['I write scalable and responsible website and applications',
+              'My communication skills are pretty amazing',
+              'I enjoy skateboarding',
+              "I watch comedy when I'm not coding"
+            ],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop:      true 
+  });
+}
